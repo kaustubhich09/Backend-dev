b6fadfc3c82a8943eafe083dc853383f2d620cb6
kaustubhi <kaustubhichauhan41@gmail.com>
2026-01-22 11:26:03 +0530

    [kaustubhi chauhan] add question2 file

---
+++ assignment/main.js
@@ -0,0 +1,6 @@
+// Exercise 2
+const stringUtils = require("./que2");
+
+console.log(stringUtils.capitalize("hello world"));
+console.log(stringUtils.reverseString("nodejs"));
+console.log("Vowels:", stringUtils.countVowels("JavaScript"));
